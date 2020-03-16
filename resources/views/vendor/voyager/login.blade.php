959c07c6834a241ea05573d1cdfd86623f2a5e47
Harun-or-rashid <harun35-2093@diu.edu.bd>
2020-03-16 19:35:15 +0600

    forgot button added

--- resources/views/vendor/voyager/login.blade.php
+++ resources/views/vendor/voyager/login.blade.php
@@ -86,10 +86,16 @@
 
                     <div class="form-group" id="rememberMeGroup">
                         <div class="controls">
-                        <input type="checkbox" name="remember" id="remember" value="1"><label for="remember" class="remember-me-text">{{ __('voyager::generic.remember_me') }}</label>
+                            <input type="checkbox" name="remember" id="remember" value="1"><label for="remember" class="remember-me-text">{{ __('voyager::generic.remember_me') }}</label>
+                        </div>
+
+                        <div class="controls">
+                            <a href="#" style="color: red;" name="forgot" id="forgot" value="1">
+                              {{ __('Forgot Password') }}
+                            </a>
                         </div>
                     </div>
-                    
+
                     <button type="submit" class="btn btn-block login-button">
                         <span class="signingin hidden"><span class="voyager-refresh"></span> {{ __('voyager::login.loggingin') }}...</span>
                         <span class="signin">{{ __('voyager::generic.login') }}</span>
